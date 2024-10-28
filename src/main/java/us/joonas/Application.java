16d90a479350da4627e23839295b0142e9d36ab8
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2024-10-28 08:59:27 +0100

    Fix for incorrect login URLs

--- src/main/java/us/joonas/Application.java
+++ src/main/java/us/joonas/Application.java
@@ -1,9 +1,11 @@
 package us.joonas;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.spring.security.NavigationAccessControlConfigurer;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
@@ -18,5 +20,12 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    static NavigationAccessControlConfigurer navigationAccessControlConfigurerCustomizer() {
+        return new NavigationAccessControlConfigurer()
+                .withAnnotatedViewAccessChecker()
+                .withLoginView("/oauth2/authorization/control-center");
     }
 }
